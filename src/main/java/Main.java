68a85a7b0c52cb3ecc24b35ff064c583721987bf
Buğra Öztürk <ozturkbugra93@gmail.com>
2019-11-27 22:39:00 +0300

    -- Results Seems Good

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,15 +7,13 @@ public class Main
         DataGenerator dataGenerator = new DataGenerator();
         dataGenerator.generateData(10, 2, 0, 0,
                 10000, false, "test");
+
         HT ht = new HT();
         ht.name = "HT";
-//        ht.configure(0, 0.2F, 0.5F, 10, false, null);
-//        ht.configure(0, 10000, 3, true, 0.1F, 0.000000001F);
         ht.train(dataGenerator.getInstances(), dataGenerator.getInstanceExamples());
 
         NB nb = new NB();
         nb.name = "nb";
-//        nb.configure(15);
         nb.train(dataGenerator.getInstances(), dataGenerator.getInstanceExamples());
     }
 }
